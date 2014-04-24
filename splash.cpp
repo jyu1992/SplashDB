d2ae060792a4a9ed07642589644774b5fc1bc7a7
Jonathan Yu <jy2432@columbia.edu>
2014-04-24 08:44:36 -0400

    Implement reading user input

--- splash.cpp
+++ splash.cpp
@@ -1,12 +1,65 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <string>
+#include <cstdlib>
 
 #include "splash_table.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
-  SplashTable st(4, 32, 1, 8);
-  st.put(3, 4);
-  st.put(4, 5);
-  std::cout << st.get(3) << '\n' << st.get(4) << '\n' << st.get(5) << std::endl;
-  return 0;
+  if (argc < 6) {
+    std::cerr << "Usage: " << argv[0] << " B R S h inputfile [dumpfile]\n"
+              << "B: bucket size (power of 2)\n"
+              << "R: maximum reinsertions\n"
+              << "S: base 2 log of capacity\n"
+              << "h: number of hash functions" << std::endl;
+    std::exit(1);
+  }
+
+  int b = atoi(argv[1]);
+  int r = atoi(argv[2]);
+  int s = atoi(argv[3]);
+  int h = atoi(argv[4]);
+
+  if (b < 1 || b & (b-1)) {
+    std::cerr << "B must be a power of 2, at least 1" << std::endl;
+    std::exit(1);
+  }
+
+  if (r < 0) {
+    std::cerr << "R cannot be negative" << std::endl;
+    std::exit(1);
+  }
+
+  if (s < 0) {
+    std::cerr << "S cannot be negative" << std::endl;
+    std::exit(1);
+  }
+
+  if ((unsigned) s > sizeof(size_t) * 8 - 1) {
+    std::cerr << "S is too large" << std::endl;
+    std::exit(1);
+  }
+
+  if (h < 1 || h > 100) {
+    std::cerr << "h must be from 1-100" << std::endl;
+    std::exit(1);
+  }
+
+  std::ifstream input(argv[5]);
+  if (!input.good()) {
+    std::cerr << "input file \"" << argv[5] << "\" not found" <<std::endl;
+    std::exit(1);
+  }
+
+  SplashTable st(h, (1u << s) / b, b, r);
+
+  std::string line;
+  uint32_t key, value;
+  while (std::getline(input, line)) {
+    std::stringstream(line) >> key >> value;
+    st.put(key, value);
+  }
 }
